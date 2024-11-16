deb3948983cb25d16ccef65f55d1d4921020b722
Wallace Artur <wallacearthur_3@hotmail.com>
2024-11-16 17:51:04 -0300

    Move game on list

--- src/main/java/com/wallacearturtop/dslist/controllers/GameListController.java
+++ src/main/java/com/wallacearturtop/dslist/controllers/GameListController.java
@@ -4,11 +4,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wallacearturtop.dslist.dto.GameListDTO;
 import com.wallacearturtop.dslist.dto.GameMinDTO;
+import com.wallacearturtop.dslist.dto.replacementDTO;
 import com.wallacearturtop.dslist.services.GameListService;
 import com.wallacearturtop.dslist.services.GameService;
 
@@ -34,6 +37,12 @@ public class GameListController {
 	public List<GameMinDTO> findByList(@PathVariable Long listId) {		
 	List<GameMinDTO> result = gameService.findByList(listId);
 	return result;
+
+	}
+	
+	@PostMapping(value = "/{listId}/replacement")
+	public void move(@PathVariable Long listId,@RequestBody replacementDTO body) {		
+     gameListService.move(listId,body.getSourceIndex(),body.getDestinationIndex());	
 
 	}
 }
---
+++ src/main/java/com/wallacearturtop/dslist/dto/replacementDTO.java
@@ -0,0 +1,23 @@
+package com.wallacearturtop.dslist.dto;
+
+public class replacementDTO {
+
+	private Integer sourceIndex;
+	private Integer destinationIndex;
+	
+	
+	public Integer getSourceIndex() {
+		return sourceIndex;
+	}
+	public void setSourceIndex(Integer sourceIndex) {
+		this.sourceIndex = sourceIndex;
+	}
+	public Integer getDestinationIndex() {
+		return destinationIndex;
+	}
+	public void setDestinationIndex(Integer destinationIndex) {
+		this.destinationIndex = destinationIndex;
+	}
+	
+	
+}
